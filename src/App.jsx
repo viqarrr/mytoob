758c8bbd6dd9b3062b9e8fba6ce7655f6997a1dd
viqarrr <viqarmulya@gmail.com>
2024-02-04 21:08:06 +0700

    add navbar

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Box } from "@mui/material";
+import { Navbar, Feed, SearchFeed, VideoDetail, ChannelDetail } from "./components";
+
+const App = () => {
+  return(
+    <BrowserRouter>
+      <Box>   
+        <Navbar />
+        <Routes>
+          <Route path="/" exact element={<Feed />} />
+          <Route path="/video/:id" exact element={<VideoDetail />} />
+          <Route path="/channel/:id" exact element={<ChannelDetail />} />
+          <Route path="/search/:searchTree" exact element={<SearchFeed />} />
+        </Routes>
+      </Box>
+    </BrowserRouter>
+  )
+}
+
+export default App
